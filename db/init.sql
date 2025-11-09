146457878f1e3139b45a1c3f8d0f80fffd23bdbd
Anastasia=Storozhenko <nsgen06@gmail.com>
2025-11-10 01:56:30 +0200

    a

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,10 @@
+CREATE DATABASE IF NOT EXISTS userstory;
+USE userstory;
 CREATE TABLE IF NOT EXISTS projects (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  description VARCHAR(255),
-  name VARCHAR(255),
-  PRIMARY KEY (id)
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    PRIMARY KEY (id)
 );
+INSERT IGNORE INTO projects (name, description) VALUES ('Test Project', 'Description');
+SELECT 'Data inserted successfully' AS message, (SELECT COUNT(*) FROM projects) AS project_count;
