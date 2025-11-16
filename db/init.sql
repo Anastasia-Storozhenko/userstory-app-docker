0b52579a0bdbed7f7de5851a68ea92628f74d888
Anastasia-Storozhenko <nsgen06@gmail.com>
2025-11-16 17:07:00 +0200

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,10 @@
 CREATE TABLE IF NOT EXISTS projects (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  description VARCHAR(255),
-  name VARCHAR(255),
-  PRIMARY KEY (id)
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    PRIMARY KEY (id)
 );
+
+INSERT INTO projects (name, description) VALUES
+('Project 1', 'Description for Project 1'),
+('Project 2', 'Description for Project 2');
